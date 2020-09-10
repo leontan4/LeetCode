6b56c77a74ad1e49f5cbd58ce09a44675d436183
tanxx519 <tanxx519@umn.edu>
2020-09-10 18:39:05 -0500

    Create #1-Two Sum.py

---
+++ Problem/Easy/#1-Two Sum.py
@@ -0,0 +1,21 @@
+#Brute Force Solution
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        total = 0
+
+        for i in range(len(nums)):
+
+            total = nums[i]    
+
+            for j in range(i+1, len(nums)):
+                total = nums[i]
+                total += nums[j]
+
+                if(total == target):
+                    break
+
+            if(total == target):
+                break
+            
+        return str(i), str(j)
