0a798b981918249bbef59bccacc45ea15ccf7e24
tanxx519 <tanxx519@umn.edu>
2020-09-11 16:50:20 -0500

    added solutions

--- Problem/Easy/1. TwoSum.py
+++ Problem/Easy/1. TwoSum.py
@@ -19,3 +19,15 @@ class Solution:
                 break
             
         return str(i), str(j)
+
+# Efficient Solution
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        h = {}
+        for i, num in enumerate(nums):
+            n = target - num
+            if n not in h:
+                h[num] = i
+            else:
+                return [h[n], i]
--- Problem/Easy/125. ValidPalindrome.py
+++ Problem/Easy/125. ValidPalindrome.py
@@ -1,4 +1,4 @@
-# Efficient Solution
+# Brute Force Solution
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         new_string=[character for character in s if character.isalnum()]
@@ -11,3 +11,12 @@ class Solution:
             return True
         else:
             return False
+
+# Efficient Solution    
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+	# upper case convert lower case
+	# delete punctuation and space
+	nor_str = ''.join([c.lower() if c.isalnum() else '' for c in s])
+	# two pinters check
+    return nor_str[:] == nor_str[::-1]
