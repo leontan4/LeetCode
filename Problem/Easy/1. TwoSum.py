322a0f82220ddf51d302849ef0998a431640f3df
tanxx519 <tanxx519@umn.edu>
2020-09-14 22:33:32 -0500

    added comments and modified code efficiency

--- Problem/Easy/1. TwoSum.py
+++ Problem/Easy/1. TwoSum.py
@@ -1,4 +1,4 @@
-#Brute Force Solution - O(n^2)
+# Brute Force Solution - O(n^2)
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         
@@ -23,11 +23,10 @@ class Solution:
 # Efficient Solution
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        
-        h = {}
-        for i, num in enumerate(nums):
-            n = target - num
-            if n not in h:
-                h[num] = i
-            else:
-                return [h[n], i]
+        hashmap = {}
+        for i, x in enumerate(nums):
+            y = target - x  # target = x+y
+            if y not in hashmap:
+                hashmap[x] = i  # add the current element to hashmap with it's index
+            else:  # since the other element already exists in the hashmap thus directly return the indexes of both.
+                return [hashmap[y], i] # the "y" that is returned, is to locate the "x" in the hashmap.
