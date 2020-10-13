98b1b36f0b8fe1fde7377f567aaf7640f66b7de9
tanxx519 <tanxx519@umn.edu>
2020-10-12 21:35:12 -0500

    updated and added new problems

---
+++ Problem/Easy/Java/1.Two Sums.java
@@ -0,0 +1,25 @@
+//Optimal Solution --> Time and Space O(n)
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        int x, y;
+
+        Map<Integer, Integer> calc = new HashMap<>();
+        
+        for (int i=0; i<nums.length; i++){
+            y = nums[i];
+            x = target - y;
+
+            if(calc.containsKey(x)){
+                return new int[] {calc.get(x), i};
+            }
+            
+            else {
+                calc.put(y, i);
+            }
+        }
+        
+        throw new IllegalArgumentException("No two sum solution");
+    
+    }
+}
