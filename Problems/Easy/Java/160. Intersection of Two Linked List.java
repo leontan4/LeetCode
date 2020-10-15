c7bb97d0a2b4bf39dafe793eaf05898c42c6a84a
tanxx519 <tanxx519@umn.edu>
2020-10-14 22:45:29 -0500

    updated tracker

--- Problems/Easy/Java/160. Intersection of Two Linked List.java
+++ Problems/Easy/Java/160. Intersection of Two Linked List.java
@@ -1,3 +1,24 @@
+// Shorter way of writing
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+              
+        ListNode x = headA, y=headB;
+        
+        if(x == null || y==null){
+            return null;
+        }
+        
+        while(x != y){
+            
+            x = x == null ? headB : x.next;
+            y = y == null ? headA : y.next;
+        }
+        
+        return x;
+        
+    }
+}
+
 // Optimal Solution
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
