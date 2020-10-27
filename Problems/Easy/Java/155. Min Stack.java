d1757c5f40e02d56e64d11462c7bb647ca6906cc
tanxx519 <tanxx519@umn.edu>
2020-10-26 22:57:49 -0500

    added solutions

---
+++ Problems/Easy/Java/155. Min Stack.java
@@ -0,0 +1,40 @@
+// Optimal Solution - Using two stacks
+class MinStack {
+
+    /** initialize your data structure here. */
+    Stack<Integer> stack = new Stack();
+    Stack<Integer> min_vals = new Stack();
+    
+    public void push(int x) {
+        if(min_vals.isEmpty() || x <= min_vals.peek()){
+        min_vals.push(x);
+        }
+        
+        stack.push(x);
+    }
+    
+    public void pop() {
+        if(stack.peek().equals(min_vals.peek())){
+            min_vals.pop();
+        }
+        stack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return min_vals.peek();
+        
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
