bf0d0647056eb438aa3c74a9e2ac752a2da5562d
Leon Tan <tanxx519@umn.edu>
2020-12-23 14:39:48 -0600

    added problems

---
+++ Problems/Easy/Java/1450. Number of Students Doing Homework at a Given Time.java
@@ -0,0 +1,12 @@
+// Brute Force
+class Solution {
+    public int busyStudent(int[] startTime, int[] endTime, int queryTime) {
+        int count = 0;
+        for(int i=0; i<startTime.length; i++){
+            if(endTime[i]>= queryTime && startTime[i] <= queryTime){
+                count++;
+            }
+        }        
+        return count;
+    }
+}
