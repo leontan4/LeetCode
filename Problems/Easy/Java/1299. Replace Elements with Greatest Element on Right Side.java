66b66d577a37ecee074fc7ba647c0a764424379b
Leon Tan <tanxx519@umn.edu>
2021-01-04 13:49:18 -0600

    updated problems

--- Problems/Easy/Java/1299. Replace Elements with Greatest Element on Right Side.java
+++ Problems/Easy/Java/1299. Replace Elements with Greatest Element on Right Side.java
@@ -1,4 +1,24 @@
-//Bute Force
+//Optimal Solution - Time O(n), Space O(1)
+// Two Pointers
+class Solution {
+    public int[] replaceElements(int[] arr) {
+        int currMax = arr[arr.length-1];
+        for(int i=arr.length-2; i>=0; i--){
+            if(arr[i] >= currMax){
+                int prevMax = currMax;
+                currMax = arr[i];
+                arr[i] = prevMax;
+            } else{
+                arr[i] = currMax;
+            }
+        }
+
+        arr[arr.length-1] = -1;
+        return arr;
+    }
+}
+
+//Brute Force
 class Solution {
     public int[] replaceElements(int[] arr) {
         for (int i=0; i<arr.length-1; i++){
