1e5e6815a8fcc5b1dd348e568e3f467a16e98ce1
Leon Tan <tanxx519@umn.edu>
2021-02-12 14:39:42 -0600

    updated solution

--- Problems/Easy/977. Squares of Sorted Array.java
+++ Problems/Easy/977. Squares of Sorted Array.java
@@ -1,5 +1,26 @@
-//Brute Force
-//Sorting
+// Optimal Solution - Time O(n), Space O(1)
+// Two Pointers
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int[] squaredArr = new int[nums.length];
+        int left=0, right=nums.length-1, k=right;
+
+        while(left <= right){
+            if(Math.abs(nums[left]) > nums[right]){
+                squaredArr[k--] = nums[left] * nums[left];
+                left++;
+            }
+            else {
+                squaredArr[k--] = nums[right] * nums[right];
+                right--;
+            }
+        }
+        return squaredArr;
+    }
+}
+
+// Brute Force - Time O(nlogn), Space O(1)
+// Sorting
 class Solution {
     public int[] sortedSquares(int[] arr) {
         for(int i=0; i<arr.length; i++){
