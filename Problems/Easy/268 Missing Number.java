8e978f6142bac3999e5a2493ed44d08d030eb9eb
tanxx519 <tanxx519@umn.edu>
2021-02-21 15:21:43 -0600

    updated solution

--- Problems/Easy/268 Missing Number.java
+++ Problems/Easy/268 Missing Number.java
@@ -1,5 +1,60 @@
-//Brute Force
-//Solution 1 - Arrays.sort()
+// Optimal Solution - Time O(n), Space O(1)
+// Gauss' Method (Math)
+class Solution {
+    public int missingNumber(int[] nums) {        
+        int elemSum=0, indexSum=nums.length;;
+        for(int i=0; i<nums.length; i++){
+            elemSum += nums[i];
+            indexSum += i;
+        }
+        return indexSum - elemSum;
+    }
+}
+
+// Optimal Solution - Time O(n), Space O(1)
+// Bit Manipulation
+class Solution {
+    public int missingNumber(int[] nums) {        
+        int missing=nums.length;
+        for(int i=0; i<nums.length; i++){
+            missing ^= i ^ nums[i];
+        }
+        return missing;
+    }
+}
+
+// Better Solution - Time O(n), Space O(1)
+// Cyclic Sort
+class Solution {
+    public int missingNumber(int[] nums) {        
+        int i = 0;
+        
+        while(i < nums.length){
+            if(nums[i] < nums.length && nums[i] != nums[nums[i]]){
+                swap(nums, i, nums[i]);
+            }
+            else {
+                i++;
+            }
+        }
+        
+        for(int j=0; j<nums.length; j++){
+            if(j != nums[j]){
+                return j;
+            }
+        }
+        return nums.length;
+    }
+    
+    private static void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
+
+// Brute Force 1 - Time O(n), Space O(n)
+// Arrays.sort()
 class Solution {
     public int missingNumber(int[] nums) {
         int count = 0;
@@ -16,7 +71,8 @@ class Solution {
     }
 }
 
-//Solution 2- "bucket" method
+// Brute Force 2- Time O(n), Space O(n)
+// "Bucket" method
 class Solution {
     public int missingNumber(int[] nums) {
         int[] res = new int[100001];
