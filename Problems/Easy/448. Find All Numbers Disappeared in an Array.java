657edd9d0d572958a2d83794f55901c7c4436696
tanxx519 <tanxx519@umn.edu>
2021-02-21 14:41:35 -0600

    updated solution

--- Problems/Easy/448. Find All Numbers Disappeared in an Array.java
+++ Problems/Easy/448. Find All Numbers Disappeared in an Array.java
@@ -1,4 +1,38 @@
-//Brute Force - Time o(n), Space O(n)
+// Optimal Solution - Time O(n), Space O(1)
+// Cyclic Sort
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List <Integer> result = new ArrayList<>();
+        if (nums == null || nums.length == 0) return result;
+        
+        int i=0;
+        while(i < nums.length){
+            int j = nums[i] - 1;
+            if(nums[i] != nums[j]){
+                swap(nums, i, j);
+            }
+            else {
+                i++;
+            }
+        }
+        
+        for(int k=0; k<nums.length; k++){
+            if(k != nums[k] - 1){
+                result.add(k+1);
+            }
+        }
+        return result;
+    }
+    
+    private static void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
+
+//Brute Force - Time O(n), Space O(n)
+// HashMap
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
         Map<Integer, Integer> arr = new HashMap<>();
