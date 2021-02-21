d2293919a2b888bfa44234ae9e822f438ae98be0
tanxx519 <tanxx519@umn.edu>
2021-02-21 08:46:42 -0600

    added solution

---
+++ Problems/Medium/287. Find the Duplicate Number.java
@@ -0,0 +1,18 @@
+// Brute Force - Time O(n), Space O(n)
+// HashMap
+class Solution {
+    public int findDuplicate(int[] nums) {
+        
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for(int i=0; i<nums.length; i++){
+            if(map.containsKey(nums[i])){
+                return nums[i];
+            }
+            else{
+                map.put(nums[i], 1);
+            }
+        }
+        return 0;
+    }
+}
