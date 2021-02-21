82d0a9831c3e48ab2c60fa59a719d60eb1abfb23
tanxx519 <tanxx519@umn.edu>
2021-02-21 16:28:53 -0600

    updated solution

--- Problems/Medium/287. Find the Duplicate Number.java
+++ Problems/Medium/287. Find the Duplicate Number.java
@@ -1,3 +1,30 @@
+// Optimal Solution - Time O(n), Space O(1)
+// Cyclic Sort
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int i=0;
+        while(i < nums.length){
+            int j=nums[i] - 1;
+            if(nums[i] != nums[j]){
+                swap(nums, i, j);
+            }
+            else if(i != j && nums[i] == nums[j]){
+                return nums[j];
+            }
+            else {
+                i++;
+            }
+        }
+        return -1;
+    }
+    
+    private static void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
+
 // Brute Force - Time O(n), Space O(n)
 // HashMap
 class Solution {
--- Problems/Medium/442. Find All Duplicates in an Array.java
+++ Problems/Medium/442. Find All Duplicates in an Array.java
@@ -1,3 +1,39 @@
+// Optimal Solution - Time O(n), Space O(1)
+// Cyclic Sort
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        List <Integer> list = new ArrayList<>();
+        
+        int i = 0;
+        while(i < nums.length){
+            int j = nums[i] - 1;
+            
+            if(nums[i] != nums[j]){
+                swap(nums, i, j);
+            }
+            else if(i != j && nums[i] == nums[j]){
+                i++;
+            } 
+            else {
+                i++;
+            }
+        }
+        
+        for(int k=0; k<nums.length; k++){
+            if(nums[k] != k+1){
+                list.add(nums[k]);
+            }
+        }
+        return list;
+    }
+    
+    private static void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
+
 //Brute Force - Time O(n), Space O(n)
 // HashMap
 class Solution {
