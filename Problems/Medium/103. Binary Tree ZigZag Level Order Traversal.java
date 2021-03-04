eafeb738f23df711316bc5e4cab640bb87e845c3
tanxx519 <tanxx519@umn.edu>
2021-03-03 19:09:45 -0600

    updated optimal solution

--- Problems/Medium/103. Binary Tree ZigZag Level Order Traversal.java
+++ Problems/Medium/103. Binary Tree ZigZag Level Order Traversal.java
@@ -13,8 +13,44 @@
  *     }
  * }
  */
+// Better Solution - Time O(n), Space O(n)
+// Breadth First Search + Queue + Boolean
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        
+        List<List<Integer>> outer = new LinkedList<>();
+        if(root == null) return outer;
+        
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        boolean isReversal = false;
+        
+        while(!q.isEmpty()){
+            int size = q.size();
+            List<Integer> inner = new ArrayList<>();
+            
+            for(int i=0; i<size; i++){
+                TreeNode curr = q.poll();    
+                
+                if(isReversal){
+                    inner.add(0, curr.val);
+                } else{
+                    inner.add(curr.val);
+                }
+                
+                if(curr.left != null) q.add(curr.left);
+                if(curr.right != null) q.add(curr.right);
+            }
+            
+            outer.add(inner);
+            isReversal = !isReversal;
+        }
+        return outer;
+    }
+}
+
 // Brute Force - Time O(n), Space O(n)
-// Breadth First Search + Queue
+// Breadth First Search + Queue + Count
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         
