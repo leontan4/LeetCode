4a982fa761ea5213999c071fa1d03494dc5874e8
tanxx519 <tanxx519@umn.edu>
2021-03-03 20:04:24 -0600

    added Brute Force

---
+++ Problems/Medium/515. Find Largest Value in Each Row.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+// Brute Force - Time O(n), Space O(n)
+// Breadth First Search + Queue + Math.max
+class Solution {
+    public List<Integer> largestValues(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if(root == null) return result;
+        
+        Queue <TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        
+        while(!q.isEmpty()){
+            int size = q.size();
+            int max = Integer.MIN_VALUE;
+            
+            for(int i=0; i<size; i++){
+                TreeNode curr = q.poll();
+                max = Math.max(max, curr.val);
+                
+                if(curr.left != null) q.add(curr.left);
+                if(curr.right != null) q.add(curr.right);
+                
+            }
+            result.add(max);
+        }
+        return result;
+    }
+}
