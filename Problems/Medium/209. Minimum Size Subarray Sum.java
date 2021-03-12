a8953aaef96702ac2c42e1da06233b8fd15780b8
tanxx519 <tanxx519@umn.edu>
2021-03-12 10:11:45 -0600

    first commit solution

---
+++ Problems/Medium/209. Minimum Size Subarray Sum.java
@@ -0,0 +1,18 @@
+// Brute Force = Time O(n), Space O(n)
+// Sliding Window
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int min = Integer.MAX_VALUE, winStart = 0, sum = 0;
+        
+        for(int winEnd = 0; winEnd < nums.length; winEnd++){
+            sum += nums[winEnd];
+            
+            while(sum >= target && winStart <= winEnd){
+                min = Math.min(min, winEnd - winStart + 1);
+                sum -= nums[winStart++];
+            }
+            if(winEnd - winStart == nums.length - 1 && sum < target) return 0;
+        }
+        return min;
+    }
+}
