23601b6880293db697694650fa18a6b4e9ab3d28
tanxx519 <tanxx519@umn.edu>
2021-03-12 17:48:21 -0600

    updated solution

--- Problems/Medium/3. Longest Substring Without Repearing Characters.java
+++ Problems/Medium/3. Longest Substring Without Repearing Characters.java
@@ -1,3 +1,32 @@
+// Optimal Solution - Time O(n), Space O(n)
+// HashMap + Sliding Window + Dynamic Programming
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        if(s.length() < 1) return 0;
+        HashMap <Character, Integer> map = new HashMap<>();
+        int winStart = 0, max = Integer.MIN_VALUE; 
+        
+        for(int winEnd = 0; winEnd < s.length(); winEnd++){
+            char rightChar = s.charAt(winEnd);
+            
+            // if map already contains the "rightChar", shrink the window from the beginning
+            // so that we have only one occurence of "rightChar"
+            if(map.containsKey(rightChar)){
+                
+                // this is tricky; in the current window, we will not have any "rightChar" after its previous index
+                // and if "winStart" is already ahead of the last index of "rightChar", we'll keep "winStart"
+		// basically restart the counting for unique characters
+		// and reposition "winStart" before/at where "winEnd" first found the duplicate element
+                winStart = Math.max(winStart, map.get(rightChar) + 1); 
+            }
+	    // Instead of storing the count of occurence, we store the "index" of elements
+            map.put(rightChar, winEnd);
+            max = Math.max(max, winEnd - winStart + 1);
+        }
+        return max;
+    }
+}
+
 // Brute Force - Time O(n), Space O(n)
 // HashMap + Sliding Window
 class Solution {
